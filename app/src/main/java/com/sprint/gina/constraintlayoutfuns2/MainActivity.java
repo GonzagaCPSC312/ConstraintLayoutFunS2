98d4560260be3002ab60d362e18840558f8dd9a3
Gina Sprint <sprint@gonzaga.edu>
2021-10-19 08:08:08 -0700

    skeleton icon credit

--- app/src/main/java/com/sprint/gina/constraintlayoutfuns2/MainActivity.java
+++ app/src/main/java/com/sprint/gina/constraintlayoutfuns2/MainActivity.java
@@ -7,6 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+// skeleton icon credit:
+// <div>Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
